cdccd7e0aee5ad14019197d4c95993fbc7d82fff
Misha Biryuk <vovann7093@gmail.com>
2021-07-21 16:25:19 +0300

    first commit

---
+++ Pizza order 2.0/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace Pizza_Order_2._0
+{
+    class Program
+    {
+        const int MaxUpperLimit = 5;
+        const int DoughUnleavened = 1;
+        const int DoughYeast = 2;
+
+
+
+
+        static void Main(string[] args)
+        {
+            static void Exit()
+            {
+                Environment.Exit(0);
+            }
+            
+
+            Console.WriteLine("Вибер1ть з якого т1ста буде складатись п1ца\n1. Бездр1жджове\n2. Др1жджове\n3. Листкове\n4. Чорне\n0. Вих1д");
+          
+            for (int i = 0; i < Int32.MaxValue; i++)
+            {
+
+                
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //for (; dough < MaxUpperLimit; dough++)
+            //{
+            //    //if (i == DoughUnleavened)
+            //    //    break;
+            //    //dough = "Бездр1жджове";
+            //    switch (dough)
+            //    {
+            //        case 1:
+            //            string dough1 = "Ковбаса";
+            //            break;
+            //        case 2:
+            //            string dough2 = "Курка";
+            //            break;
+            //        case 3:
+            //            string dough3 = "Бекон";
+            //            break;
+            //        case 4:
+            //            string dough4 = "Сир";
+            //            break;
+            //        case 0:
+            //            Exit();
+            //            break;
+            //    }
+            //}
+
+
+                //Console.WriteLine("Вибер1ть яка начинка буде у п1ц1\n1. Ковбаса\n2. Курка\n3. Бекон\n4. Сир\n0. Вих1д");
+                //string filling = Console.ReadLine();
+                //switch (filling)
+                //{
+                //    case "1":
+                //        filling = "Ковбаса";
+                //        break;
+                //    case "2":
+                //        filling = "Курка";
+                //        break;
+                //    case "3":
+                //        filling = "Бекон";
+                //        break;
+                //    case "4":
+                //        filling = "Сир";
+                //        break;
+                //    case "0":
+                //        Exit();
+                //        break;
+                //}
+                //Console.Clear();
+
+                //Console.WriteLine("Вибер1ть який соус буде у п1ц1\n1. Кетчуп\n2. Майонез\n3. Тартар\n4. Сирний\n0. Вих1д");
+                //string sauce = Console.ReadLine();
+                //switch (sauce)
+                //{
+                //    case "1":
+                //        sauce = "Кетчуп";
+                //        break;
+                //    case "2":
+                //        sauce = "Майонез";
+                //        break;
+                //    case "3":
+                //        sauce = "Тартар";
+                //        break;
+                //    case "4":
+                //        sauce = "Сирний";
+                //        break;
+                //    case "0":
+                //        Exit();
+                //        break;
+                //}
+                //Console.Clear();
+
+                //Console.WriteLine("Вибер1ть як1 додатков1 1нгред1єнти будуть у п1ц1\n1. Оливки\n2. Пом1дори\n3. Ананас\n4. Креветки\n0. Вих1д");
+                //string additional = Console.ReadLine();
+                //switch (additional)
+                //{
+                //    case "1":
+                //        additional = "Оливки";
+                //        break;
+                //    case "2":
+                //        additional = "Пом1дори";
+                //        break;
+                //    case "3":
+                //        additional = "Ананас";
+                //        break;
+                //    case "4":
+                //        additional = "Креветки";
+                //        break;
+                //    case "0":
+                //        Exit();
+                //        break;
+                //}
+                //Console.Clear();
+
+                Console.WriteLine($"Ваша п1ца");
+            Console.WriteLine($"Т1сто:  ");
+        //    Console.WriteLine($"Начинка: {filling}");
+        //    Console.WriteLine($"Соус: {sauce}");
+        //    Console.WriteLine($"Додатков1 1нгред1єти: {additional}");
+        }
+    }
+}
